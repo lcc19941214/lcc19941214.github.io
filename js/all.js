6f430d59edf68926c6d2024f0d2e209cebf0e25d
Conan <245987239@qq.com>
2015-07-16 00:24:52 +0800

    upload website
    upload website for the first time

---
+++ js/all.js
@@ -0,0 +1,41 @@
+function showmenu () {
+	var oml=document.getElementById('menulist');
+
+	oml.style.display="block";
+}
+function hidemenu () {
+	var oml=document.getElementById('menulist');
+
+	oml.style.display="none";
+}
+/*
+function itemchange () {
+	var i1=document.getElementById('item1');
+
+	i1.style.width="100%";
+}
+ */
+function showwtcode () {
+	// 显示微信二维码
+	var owtcode=document.getElementById("wtcode");
+
+	owtcode.style.display="block";
+}
+function hidewtcode () {
+	// 显示微信二维码
+	var owtcode=document.getElementById("wtcode");
+
+	owtcode.style.display="none";
+}
+function showqqcode () {
+	// 显示微信二维码
+	var oqqcode=document.getElementById("qqcode");
+
+	oqqcode.style.display="block";
+}
+function hideqqcode () {
+	// 显示微信二维码
+	var oqqcode=document.getElementById("qqcode");
+
+	oqqcode.style.display="none";
+}
